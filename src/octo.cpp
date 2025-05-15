cab734e03549dd6573716bac01f41ffaf93dd121
Israel Bautista <isra.bautista@gmail.com>
2025-05-15 22:49:02 +0200

    Prueba

--- src/octo.cpp
+++ src/octo.cpp
@@ -1,5 +1,11 @@
 #include "octo.h"
 
+#ifdef _WIN32
+ 
+template class OCTOAPI std::vector<Octo,std::allocator<Octo>>;
+
+#endif // OS
+
 std::string Octo::dame() {
   using namespace std::string_literals;
   return "mipan"s;
